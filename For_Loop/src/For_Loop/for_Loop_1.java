2a65cf874241360d9236ef49a42a2e5f775d392e
Leonardo Cabano <85898516+lcabano@users.noreply.github.com>
2021-11-08 03:00:36 -0300

    Add files via upload
    Imprimo numeros desde el 1 al 9

---
+++ For_Loop/src/For_Loop/for_Loop_1.java
@@ -0,0 +1,17 @@
+package For_Loop;
+
+public class for_Loop_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Imprimo números del 1 al 9");
+		for(int i=1; i<10; i++)
+		{
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
