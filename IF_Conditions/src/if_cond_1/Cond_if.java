533f9c0b49219b31444a7790f6ad752971c0268a
Leonardo Cabano <85898516+lcabano@users.noreply.github.com>
2021-11-08 02:01:50 -0300

    Add files via upload

---
+++ IF_Conditions/src/if_cond_1/Cond_if.java
@@ -0,0 +1,18 @@
+package if_cond_1;
+
+public class Cond_if {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola");
+		int num1=100;
+		int num2=200;
+		if(num1>num2) {
+			System.out.println("num1 es mayor");
+		}else {System.out.println("num1 es menor");}
+		
+	}
+
+}
