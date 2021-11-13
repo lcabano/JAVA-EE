5a16fc028271c1a30238eca2fc433c5c2c912084
Leonardo Cabano <85898516+lcabano@users.noreply.github.com>
2021-11-13 01:16:35 -0300

    Add files via upload

---
+++ String_Comparison/src/string_comparison/String_Comparison.java
@@ -0,0 +1,23 @@
+package string_comparison;
+
+public class String_Comparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String str1="ABCD";
+String str2="abcd";
+
+System.out.println(str1.equalsIgnoreCase(str2));
+		
+	if(str1.equalsIgnoreCase(str2)) {
+		System.out.println("Son iguales");
+	}else {
+		System.out.println("No son iguales");
+	}
+	
+	
+	}//
+
+}//
+
