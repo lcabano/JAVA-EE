04f8f958f77adc9e0a1bb08f4719de934fc2f372
Leonardo Cabano <85898516+lcabano@users.noreply.github.com>
2021-11-13 00:45:29 -0300

    Add files via upload

---
+++ For anidado/src/for_anidado/For_anidado.java
@@ -0,0 +1,19 @@
+package for_anidado;
+
+public class For_anidado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=6;i++) {
+			
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("\n");
+		}
+		
+		
+	}
+
+}
